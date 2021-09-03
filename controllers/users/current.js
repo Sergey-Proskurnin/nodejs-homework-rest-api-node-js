028b651caa9493333d3f2180ddca444eba44fb31
Sergey-Proskurnin <proskurnin@ua.fm>
2021-09-03 23:07:08 +0300

    Add minimum viable product

--- controllers/users/current.js
+++ controllers/users/current.js
@@ -1,14 +1,11 @@
-// const Users = require('../../repositories/users');
 const {
-  HttpCode: {
-    //  UNAUTHORIZED,
-      OK },
+  HttpCode: { OK },
 } = require('../../helpers');
 
 const current = async (req, res, next) => {
   try {
     const { email, subscription } = req.user;
-        return res.status(OK).json({
+    return res.status(OK).json({
       status: 'success',
       code: OK,
       user: { email, subscription },
@@ -17,26 +14,5 @@ const current = async (req, res, next) => {
     next(error);
   }
 };
-// const current = async (req, res, next) => {
-//   try {
-//     const id = req.user.id;
-//     const { email, subscription } = req.user;
-//     const currentUser = await Users.findById(id);
-//     if (!currentUser) {
-//       return res.status(UNAUTHORIZED).json({
-//         status: 'error',
-//         code: UNAUTHORIZED,
-//         message: 'Not authorized',
-//       });
-//     }
-//     return res.status(OK).json({
-//       status: 'success',
-//       code: OK,
-//       user: { email, subscription },
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 
 module.exports = current;
